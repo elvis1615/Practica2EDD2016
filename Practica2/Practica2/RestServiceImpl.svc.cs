4bbd7878ad4bb4d68324400bc7921fe740e0934a
elvis1615 <elvis_rolando@hotmail.es>
2016-04-17 23:36:54 -0600

    arvhivo
    solo hace split y lo recorre aun no entiendo como abrir el filechooser

--- Practica2/Practica2/RestServiceImpl.svc.cs
+++ Practica2/Practica2/RestServiceImpl.svc.cs
@@ -16,5 +16,30 @@ namespace Practica2
             //throw new NotImplementedException();
             return "hola k ase>>" + id +" "+a;
         }
+
+        public void leer(string ruta)
+        {
+            string line = "";
+            string date = "";
+            string device = "";
+            double dayRank = 0;
+            double totalRank = 0;
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader(ruta);
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] entradas = line.Split(',');
+                date = entradas[0];
+                device = entradas[1];
+                dayRank = Convert.ToDouble(entradas[2]);
+                totalRank = Convert.ToDouble(entradas[3]);
+                /*
+                aqui se insertan los valores en los arboles
+                */
+
+            }
+
+            file.Close();
+        }
     }
 }
